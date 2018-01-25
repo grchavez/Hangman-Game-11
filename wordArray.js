2bbcfe9e83ce45bb579d97d278bca6f0c3c72b8f
gabe <grchavez@email.arizona.edu>
2018-01-25 09:23:13 -0700

    Created new file.

--- wordArray.js
+++ wordArray.js
@@ -4,13 +4,14 @@ var Word = require("./word.js");
 module.exports = {
 
     selectWord : function(){
-        var words = ["Vince McMahon", "The Rock", "HHH", "Stone Cold Steve Austin", "Shawn Michaels"
+        var words = ["Vince McMahon", "The Rock", "HHH", "Stone Cold Steve Austin", "Shawn Michaels",
                      "Bret Hart", "Hulk Hogan", "Macho Man Randy Savage", "The Undertaker", "John Cena",
                      "Kevin Nash", "Scott Hall", "The Ultimate Warrior", "Brock Lesnar", "AJ Styles", 
                      "Kane"];
         var num = Math.floor(Math.random() * 16);
         var word = new Word(words[num]);
         return word; 
+        console.log("-------------------- SUCCESS --------------------")
 
     }
 };
